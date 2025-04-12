f1b367c4467ce8dcc8b54cfe4d6aba5f4be88f5a
Jiggly Balls <jigglyballs9000@gmail.com>
2025-04-13 03:55:10 +0530

    Updated enums and added constants

--- core/config.py
+++ core/config.py
@@ -1,8 +1,8 @@
-from enum import IntEnum
+from enum import Enum, IntEnum, auto
 
-
+TITLE: str = "Sun Strategy"
 FPS: int = 60
-
+PIXEL_SIZE: int = 32
 SCREEN_WIDTH: int = 1366
 SCREEN_HEIGHT: int = 768
 
@@ -10,4 +10,14 @@ SCREEN_HEIGHT: int = 768
 class Layers(IntEnum):
     MAIN = 1
     GROUND = 2
-    SOIL = 3
+    WATER = 3
+    WATER_ROCKS = 4
+
+
+class AssetIdentifier(Enum):
+    TERRAIN_ELEVATION = auto()
+    TERRAIN_FLAT = auto()
+    WATER = auto()
+    ROCKS = auto()
+    FOAM = auto()
+    TREES = auto
